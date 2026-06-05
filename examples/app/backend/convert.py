0be06246fc7c2822dd53a357e7e83182d79003f2
laisc <ybarros@biatech.com>
2026-06-05 00:48:24 -0300

    removing unecessary files

--- examples/app/backend/convert.py
+++
@@ -1,8 +0,0 @@
-import FreeCAD
-import Mesh
-
-doc = FreeCAD.open(r"C:\Users\Yan\Documents\Projetos\PINNeAPPle\examples\app\backend\chassis SLDPRT.SLDPRT")
-
-obj = doc.Objects[0]
-
-Mesh.export([obj], r"C:\Users\Yan\Documents\Projetos\PINNeAPPle\examples\app\backend\chassis.stl")
